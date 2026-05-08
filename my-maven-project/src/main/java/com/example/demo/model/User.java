6fe4760b7123e737aa9b96314c5147536d3c7ce6
Jyoti Bala Sahu <35549456+JyotiBalaSahu@users.noreply.github.com>
2026-05-08 16:16:55 +0000

    Class structure

--- my-maven-project/src/main/java/com/example/demo/model/User.java
+++ my-maven-project/src/main/java/com/example/demo/model/User.java
@@ -8,8 +8,27 @@ public class User{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Strig email;
+    private String email;
     
     
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
